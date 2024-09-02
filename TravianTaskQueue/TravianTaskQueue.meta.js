97bd1368ed7c330220815e500d5326b292490b20
MRB05 <48975769+MRB05@users.noreply.github.com>
2024-09-02 14:16:33 +0700

    Add files via upload

---
+++ TravianTaskQueue/TravianTaskQueue.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Travian Task Queue for Travian 4
+// @namespace   https://github.com/adipiciu/Travian-scripts
+// @author      adipiciu (based on TTQ by Risi and further edited by Nevam and then Pimp Trizkit and Serj_LV)
+// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @description Schedule delayed constructions, upgrades and attacks.
+// @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=TTQ4+script&currency_code=EUR
+// @include     *://*.travian.*
+// @include     *://*/*.travian.*
+// @exclude     *://support.travian.*
+// @exclude     *://blog.travian.*
+// @exclude     *.css
+// @exclude     *.js
+
+// @version     2.0.15
+// ==/UserScript==
