c95c8bec669029922f0be0661445e1fb5dd7f6e1
MRB05 <48975769+MRB05@users.noreply.github.com>
2024-09-02 14:41:11 +0700

    Update TravianTaskQueue.meta.js

--- TravianTaskQueue/TravianTaskQueue.meta.js
+++ TravianTaskQueue/TravianTaskQueue.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        Travian Task Queue for Travian 4
-// @namespace   https://github.com/adipiciu/Travian-scripts
+// @namespace   https://github.com/MRB05/TravianTools
 // @author      adipiciu (based on TTQ by Risi and further edited by Nevam and then Pimp Trizkit and Serj_LV)
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @description Schedule delayed constructions, upgrades and attacks.
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=TTQ4+script&currency_code=EUR
-// @include     *://*.travian.*
-// @include     *://*/*.travian.*
-// @exclude     *://support.travian.*
-// @exclude     *://blog.travian.*
+// @include     *://*.travgame.*
+// @include     *://*/*.travgame.*
+// @exclude     *://support.travgame.*
+// @exclude     *://blog.travgame.*
 // @exclude     *.css
 // @exclude     *.js
 
