5c8b94816c8047c8f603cb84e07372f12709f79b
MRB05 <48975769+MRB05@users.noreply.github.com>
2024-09-02 14:32:36 +0700

    Update TravianWaveBuilder.meta.js

--- TravianWaveBuilder/TravianWaveBuilder.meta.js
+++ TravianWaveBuilder/TravianWaveBuilder.meta.js
@@ -5,7 +5,7 @@
 // @author         adipiciu (based on Travian wave builder 0.5 by Serj_LV)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=Travian+wave+builder+script&currency_code=EUR
-// @match          https://*.travian.com/build.php*
+// @match          https://*.travgame.com/login.php*
 
 // @version        2.10
 // ==/UserScript==
